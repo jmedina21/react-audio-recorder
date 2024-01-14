7d56025d6fac48258dc214ea8f4c8602dcc3e14c
Pablo Medina <jmedinamulet@gmail.com>
2024-01-14 13:09:03 -0500

    Update audio recorder component and styles

--- src/components/AudioRecordingComponent.tsx
+++ src/components/AudioRecordingComponent.tsx
@@ -6,7 +6,6 @@ import micSVG from "../icons/mic.svg";
 import pauseSVG from "../icons/pause.svg";
 import resumeSVG from "../icons/play.svg";
 import saveSVG from "../icons/save.svg";
-import discardSVG from "../icons/stop.svg";
 import "../styles/audio-recorder.css";
 
 const LiveAudioVisualizer = React.lazy(async () => {
@@ -23,8 +22,6 @@ const LiveAudioVisualizer = React.lazy(async () => {
  * https://github.com/samhirtarif/react-audio-recorder#combine-the-useaudiorecorder-hook-and-the-audiorecorder-component
  * @prop `audioTrackConstraints`: Takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings#instance_properties_of_audio_tracks subset} of `MediaTrackConstraints` that apply to the audio track
  * @prop `onNotAllowedOrFound`: A method that gets called when the getUserMedia promise is rejected. It receives the DOMException as its input.
- * @prop `downloadOnSavePress` If set to `true` the file gets downloaded when save recording is pressed. Defaults to `false`
- * @prop `downloadFileExtension` File extension for the audio filed that gets downloaded. Defaults to `mp3`. Allowed values are `mp3`, `wav` and `webm`
  * @prop `showVisualizer` Displays a waveform visualization for the audio when set to `true`. Defaults to `false`
  * @prop `classes` Is an object with attributes representing classes for different parts of the component
  */
@@ -33,8 +30,6 @@ const AudioRecorder: (props: Props) => ReactElement = ({
   onNotAllowedOrFound,
   recorderControls,
   audioTrackConstraints,
-  downloadOnSavePress = false,
-  downloadFileExtension = "webm",
   showVisualizer = false,
   mediaRecorderOptions,
   classes,
@@ -50,7 +45,6 @@ const AudioRecorder: (props: Props) => ReactElement = ({
     mediaRecorder,
   } =
     recorderControls ??
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     useAudioRecorder(
       audioTrackConstraints,
       onNotAllowedOrFound,
@@ -66,64 +60,13 @@ const AudioRecorder: (props: Props) => ReactElement = ({
     stopRecording();
   };
 
-  const convertToDownloadFileExtension = async (
-    webmBlob: Blob
-  ): Promise<Blob> => {
-    const FFmpeg = await import("@ffmpeg/ffmpeg");
-    const ffmpeg = FFmpeg.createFFmpeg({ log: false });
-    await ffmpeg.load();
-
-    const inputName = "input.webm";
-    const outputName = `output.${downloadFileExtension}`;
-
-    ffmpeg.FS(
-      "writeFile",
-      inputName,
-      new Uint8Array(await webmBlob.arrayBuffer())
-    );
-
-    await ffmpeg.run("-i", inputName, outputName);
-
-    const outputData = ffmpeg.FS("readFile", outputName);
-    const outputBlob = new Blob([outputData.buffer], {
-      type: `audio/${downloadFileExtension}`,
-    });
-
-    return outputBlob;
-  };
-
-  const downloadBlob = async (blob: Blob): Promise<void> => {
-    if (!crossOriginIsolated && downloadFileExtension !== "webm") {
-      console.warn(
-        `This website is not "cross-origin isolated". Audio will be downloaded in webm format, since mp3/wav encoding requires cross origin isolation. Please visit https://web.dev/cross-origin-isolation-guide/ and https://web.dev/coop-coep/ for information on how to make your website "cross-origin isolated"`
-      );
-    }
-
-    const downloadBlob = crossOriginIsolated
-      ? await convertToDownloadFileExtension(blob)
-      : blob;
-    const fileExt = crossOriginIsolated ? downloadFileExtension : "webm";
-    const url = URL.createObjectURL(downloadBlob);
-
-    const a = document.createElement("a");
-    a.style.display = "none";
-    a.href = url;
-    a.download = `audio.${fileExt}`;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-  };
-
   useEffect(() => {
     if (
       (shouldSave || recorderControls) &&
       recordingBlob != null &&
       onRecordingComplete != null
-    ) {
-      onRecordingComplete(recordingBlob);
-      if (downloadOnSavePress) {
-        void downloadBlob(recordingBlob);
-      }
+      ) {
+        onRecordingComplete(recordingBlob);
     }
   }, [recordingBlob]);
 
@@ -192,15 +135,6 @@ const AudioRecorder: (props: Props) => ReactElement = ({
         onClick={togglePauseResume}
         title={isPaused ? "Resume recording" : "Pause recording"}
         data-testid="ar_pause"
-      />
-      <img
-        src={discardSVG}
-        className={`audio-recorder-options ${
-          !isRecording ? "display-none" : ""
-        } ${classes?.AudioRecorderDiscardClass ?? ""}`}
-        onClick={() => stopAudioRecorder(false)}
-        title="Discard Recording"
-        data-testid="ar_cancel"
       />
     </div>
   );
